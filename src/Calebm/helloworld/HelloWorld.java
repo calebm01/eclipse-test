4e152d7f4711169e0ae3cf2f44e49e634250e2df
caleb.mouritsen <caleb.mouritsen@CLC-LAB209-02.TCSD.tooelesd.org>
2018-09-04 13:18:54 -0600

    first commit

---
+++ src/Calebm/helloworld/HelloWorld.java
@@ -0,0 +1,26 @@
+// hello world program
+// prints "Hello World" in the console
+package Calebm.helloworld;
+
+/**
+ * @author caleb.mouritsen
+ * September 18th, 2018
+ * hello world
+ */ 
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+	
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
